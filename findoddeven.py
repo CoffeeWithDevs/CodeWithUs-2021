37bc0c782af43672b5152baf10da7001e7b60a97
dark <dark@kali.Dark>
2021-09-30 22:31:11 +0530

    Python Program to find Odd Even Number

---
+++ findoddeven.py
@@ -0,0 +1,8 @@
+# Python Program to check if a Number is Odd or Even
+
+number = int(input(" Please Enter any Integer Value : "))
+
+if(number % 2 == 0):
+    print("{0} is an Even Number".format(number))
+else:
+    print("{0} is an Odd Number".format(number))
